c86ca2425598788ab470426db71d17eeb55f7570
Yuko Uda <yuko.uda0110@gmail.com>
2020-04-26 18:37:50 -0700

    Ask a user the ID of the product he/she would like to buy, and validate the input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,17 +10,55 @@ const connection = mysql.createConnection({
   database: 'bamazon'
 });
 
-const showAllProducts = () => {
-  connection.query('SELECT item_id, product_name, price FROM products', (err, res) => {
+const showAllProducts = (res) => {
+  console.log('------ List of Products ------');
+  for (const product of res) {
+    console.log(`${product.item_id}. ${product.product_name} - $${product.price}`);
+  }
+  console.log('\n');
+}
+
+const purchaseProduct = () => {
+  connection.query('SELECT * FROM products', (err, res) => {
     if (err) {
       throw err;
     }
 
-    for (const product of res) {
-      console.log(`${product.item_id}. ${product.product_name} - $${product.price}`);
-    }
+    showAllProducts(res);
+
+    inquirer.prompt([
+      {
+        name: 'id',
+        type: 'integer',
+        message: 'Please select the ID of the product you would like to buy.',
+        validate: (input) => {
+          const idArr = [];
+          for (const product of res) {
+            idArr.push(product.item_id);
+          }
+          if (idArr.includes(parseInt(input))) {
+            return true;
+          } else {
+            return 'There is no product which matches the ID you entered. Please type in a different ID.';
+          }
+        }
+      }
+    ]).then((answer) => {
+      console.log('Selected ID: ' + answer.id);
+    }).catch(error => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log('TtyError: ');
+        console.log(error);
+      } else {
+        // Something else when wrong
+        console.log(error);
+      }
+    });
+
   })
   connection.end();
 }
 
-showAllProducts();
+// showAllProducts();
+purchaseProduct();
