8c03abfe7a30f9d560d70f2d9e7182f3b95a4ed5
Yuko Uda <yuko.uda0110@gmail.com>
2020-04-26 18:13:06 -0700

    Create showAllProducts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,26 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+// Create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'bamazon'
+});
+
+const showAllProducts = () => {
+  connection.query('SELECT item_id, product_name, price FROM products', (err, res) => {
+    if (err) {
+      throw err;
+    }
+
+    for (const product of res) {
+      console.log(`${product.item_id}. ${product.product_name} - $${product.price}`);
+    }
+  })
+  connection.end();
+}
+
+showAllProducts();
