7e67587d5fa19e1c8bffb7e42e3e1c4ff8b313ab
Yuko Uda <yuko.uda0110@gmail.com>
2020-05-02 20:27:32 -0700

    Update bamazon-db.sql

--- bamazon-db.sql
+++ bamazon-db.sql
@@ -7,25 +7,25 @@ USE bamazon;
 CREATE TABLE products (
                           item_id INT AUTO_INCREMENT,
                           product_name VARCHAR(100) NOT NULL,
-                          department_name VARCHAR(50) NOT NULL,
+                          department_id INT NOT NULL,
                           price DECIMAL(10, 2) NOT NULL,
                           stock_quantity INT default 0,
                           product_sales DECIMAL(10, 2) default 0,
                           PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+INSERT INTO products (product_name, department_id, price, stock_quantity, product_sales)
 VALUES
-('Juicer Machine', 'Home and Kitchen', 54.99, 25, 1374.75),
-('Pressure Cooker', 'Home and Kitchen', 79.00, 30, 790),
-('Vacuum Cleaner', 'Appliances', 120.50, 10, 723),
-('Microwave', 'Appliances', 89.00, 50, 534),
-('Shampoo', 'Beauty and Personal Care', 15.50, 85, 558),
-('Conditioner', 'Beauty and Personal Care', 18.00, 80, 180),
-('Baby Wipes', 'Baby', 17.99, 150, 467.74),
-('Dipers', 'Baby', 48.00, 120, 2832.00),
-('Headphone', 'Electronics', 120.00, 35, 480),
-('Printer', 'Electronics', 250.00, 18, 250);
+('Juicer Machine', 6, 54.99, 25, 1374.75),
+('Pressure Cooker', 6, 79.00, 30, 790),
+('Vacuum Cleaner', 1, 120.50, 10, 723),
+('Microwave', 1, 89.00, 50, 534),
+('Shampoo', 3, 15.50, 85, 558),
+('Conditioner', 3, 18.00, 80, 180),
+('Baby Wipes', 2, 17.99, 150, 467.74),
+('Dipers', 2, 48.00, 120, 2832.00),
+('Headphone', 4, 120.00, 35, 480),
+('Printer', 4, 250.00, 18, 250);
 
 
 CREATE TABLE departments (
